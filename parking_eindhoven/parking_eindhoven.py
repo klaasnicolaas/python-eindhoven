a3b5c3891934a903161dcc0ce950c34dbdb08303
Klaas Schoute <klaas_schoute@hotmail.com>
2022-05-07 17:46:50 +0200

    Changes to imports and make staticmethod (#20)
    * Make the function a staticmethod
    
    * Update some project stuff

--- parking_eindhoven/parking_eindhoven.py
+++ parking_eindhoven/parking_eindhoven.py
@@ -2,14 +2,14 @@
 from __future__ import annotations
 
 import asyncio
-from collections.abc import Mapping
+import socket
 from dataclasses import dataclass
 from importlib import metadata
 from typing import Any
 
+import aiohttp
 import async_timeout
-from aiohttp.client import ClientError, ClientResponseError, ClientSession
-from aiohttp.hdrs import METH_GET
+from aiohttp import hdrs
 from yarl import URL
 
 from .exceptions import (
@@ -28,18 +28,48 @@ class ParkingEindhoven:
     parking_type: int
 
     request_timeout: float = 10.0
-    session: ClientSession | None = None
+    session: aiohttp.client.ClientSession | None = None
 
     _close_session: bool = False
-    _str_parking_type: str = ""
+
+    @staticmethod
+    async def define_type(parking_type: int) -> str:
+        """Define the parking type.
+
+        Args:
+            parking_type: The selected parking type number.
+
+        Returns:
+            The parking type as string.
+
+        Raises:
+            ParkingEindhovenTypeError: If the parking type is not valid.
+        """
+        if parking_type == 1:
+            result_type = "Parkeerplaats"
+        elif parking_type == 2:
+            result_type = "Parkeerplaats Vergunning"
+        elif parking_type == 3:
+            result_type = "Parkeerplaats Gehandicapten"
+        elif parking_type == 4:
+            result_type = "Parkeerplaats Afgekruist"
+        elif parking_type == 5:
+            result_type = "Parkeerplaats laden/lossen"
+        elif parking_type == 6:
+            result_type = "Parkeerplaats Electrisch opladen"
+        else:
+            raise ParkingEindhovenTypeError(
+                "The selected number does not match the list of parking types"
+            )
+        return result_type
 
     async def _request(
         self,
         uri: str,
         *,
-        method: str = METH_GET,
-        params: Mapping[str, Any] | None = None,
-    ) -> dict[str, Any]:
+        method: str = hdrs.METH_GET,
+        params: dict[str, Any] | None = None,
+    ) -> Any:
         """Handle a request to the Parking Eindhoven API.
 
         Args:
@@ -68,7 +98,7 @@ class ParkingEindhoven:
         }
 
         if self.session is None:
-            self.session = ClientSession()
+            self.session = aiohttp.ClientSession()
             self._close_session = True
 
         try:
@@ -85,7 +115,7 @@ class ParkingEindhoven:
             raise ParkingEindhovenConnectionError(
                 "Timeout occurred while connecting to the Parking Eindhoven API."
             ) from exception
-        except (ClientError, ClientResponseError) as exception:
+        except (aiohttp.ClientError, socket.gaierror) as exception:
             raise ParkingEindhovenConnectionError(
                 "Error occurred while communicating with the Parking Eindhoven API."
             ) from exception
@@ -98,8 +128,7 @@ class ParkingEindhoven:
                 {"Content-Type": content_type, "response": text},
             )
 
-        response_data: dict[str, Any] = await response.json(content_type=None)
-        return response_data
+        return await response.json()
 
     async def locations(self, rows: int = 10) -> list[ParkingSpot]:
         """Get all the parking locations.
@@ -115,16 +144,16 @@ class ParkingEindhoven:
             ParkingEindhovenResultsError: When no results are found.
         """
         results: list[ParkingSpot] = []
-        data = await self._request(
+        locations = await self._request(
             "search/",
             params={
                 "dataset": "parkeerplaatsen",
                 "rows": rows,
-                "refine.type_en_merk": await self.define_type(),
+                "refine.type_en_merk": await self.define_type(self.parking_type),
             },
         )
 
-        for item in data["records"]:
+        for item in locations["records"]:
             try:
                 results.append(ParkingSpot.from_json(item))
             except KeyError as exception:
@@ -132,34 +161,6 @@ class ParkingEindhoven:
         if not results:
             raise ParkingEindhovenResultsError("No parking locations were found")
         return results
-
-    async def define_type(self) -> str:
-        """Define the parking type.
-
-        Returns:
-            The parking type as string.
-
-        Raises:
-            ParkingEindhovenTypeError: If the parking type is not valid.
-        """
-        if self.parking_type == 1:
-            result_type = "Parkeerplaats"
-        elif self.parking_type == 2:
-            result_type = "Parkeerplaats Vergunning"
-        elif self.parking_type == 3:
-            result_type = "Parkeerplaats Gehandicapten"
-        elif self.parking_type == 4:
-            result_type = "Parkeerplaats Afgekruist"
-        elif self.parking_type == 5:
-            result_type = "Parkeerplaats laden/lossen"
-        elif self.parking_type == 6:
-            result_type = "Parkeerplaats Electrisch opladen"
-        else:
-            raise ParkingEindhovenTypeError(
-                "The selected number does not match the list of parking types"
-            )
-        self._str_parking_type = result_type
-        return self._str_parking_type
 
     async def close(self) -> None:
         """Close open client session."""
